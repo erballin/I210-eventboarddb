0a12e2ed14387ffa1a6d58aa4e2c9e07ae2a77f7
its3lliot <ellcrai@iu.edu>
2025-04-15 16:35:32 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,24 @@ $page_title = "EventBoardDB";
 
 include ('includes/header.php');
 ?>
+    <h2>Welcome to EventBoardDB</h2>
+<content>
+<h3>
+<p1>
+    Our goal is to allow users to find and befriend likeminded indivisuals with similar interests and hobbies.
+</p1>
+        <img src="images../speaker.jpg" class="speaker">
+    <p>
+    We offer the ability to create, view, and RSVP for upcoming events in your area.
+    </p>
+    <p>
+    Events can be found using the search function, allowing
+    for users to view details about each event and the ability to RSVP
+    </p>
 
-<h2>Welcome to EventBoardDB</h2>
+</h3>
+
+</content>
 
 <?php
 include ('includes/footer.php');
