98f2689942b7e69f5f9b18c06481395f254cc64d
its3lliot <ellcrai@iu.edu>
2025-04-15 16:45:01 -0400

    Add files via upload

---
+++ about.php
@@ -0,0 +1,26 @@
+<?php
+$page_title = "About";
+
+include ('includes/header.php');
+?>
+
+<h2>Who Are We?</h2>
+
+<h4>This website was created by Elliot Craig, Erin Ballinger, and Benjamin Egger-Torke</h4>
+
+<h2> Erin </h2>
+<h4>Created Database Schema
+<br>
+    Search and Event Details Page
+</h4>
+<h2>Elliot</h2>
+<h4>Website design & mockup
+<br>
+    Index and About Page
+</h4>
+<h2>Benjamin</h2>
+<h4>
+    Account details and Creation
+</h4>
+<?php
+include ('includes/footer.php');
