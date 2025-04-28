b99e9035efd23e62cbf8cfb27e2a422690d82a7b
its3lliot <ellcrai@iu.edu>
2025-04-28 15:07:45 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,21 +4,25 @@ $page_title = "Event Board";
 include ('includes/header.php');
 ?>
     <div class="main">Welcome to Event Board</div>
-<content>
+    <div class="home">Welcome to Event Board
 <p>
-            Our goal is to allow users to find and befriend likeminded indivisuals with similar interests and hobbies.
+                Our goal is to allow users to find and befriend likeminded indivisuals with similar interests and hobbies.
 </p>
-<img src="images/speaker.jpg" class="speaker">
+<br>
+<br>
 <p>
-    We offer the ability to create, view, and RSVP for upcoming events in your area.
+        We offer the ability to create, view, and RSVP for upcoming events in your area.
 </p>
+        <br>
+        <br>
 <p>
-    Events can be found using the search function, allowing
+        Events can be found using the search function, allowing
     for users to view details about each event and the ability to RSVP
 </p>
- 
+    </div>
+    <img src="www/css/images/logo.png" class="speaker">
 
-</content>
 
 <?php
 include ('includes/footer.php');
+
